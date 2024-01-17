ab263a9030548a8632b60af024b5049c60fc4229
goulh <goulh@163.com>
2024-01-17 21:05:48 +0800

    Update diy-mini.sh

--- diy-mini.sh
+++ diy-mini.sh
@@ -29,6 +29,7 @@ function git_sparse_clone() {
 
 # 添加额外插件
 git clone --depth=1 https://github.com/kongfl888/luci-app-adguardhome package/luci-app-adguardhome
+git clone --depth=1 https://github.com/ilxp/luci-app-ikoolproxy package/luci-app-ikoolproxy
 git clone --depth=1 https://github.com/Jason6111/luci-app-netdata package/luci-app-netdata
 # git_sparse_clone master https://github.com/syb999/openwrt-19.07.1 package/network/services/msd_lite
 
